d5abb54cf6428e2cefe2ea383c784af82454586a
cshortt <cshorttrun@gmail.com>
2019-04-11 14:05:03 -0700

    Added a small toolkit analyzer that can test the similarity between a generated password list and a reference password list

--- toolkit.py
+++ toolkit.py
@@ -1,35 +1,69 @@
 
-import collections, operator
+# https://pypi.org/project/python-Levenshtein/
+import Levenshtein
+import collections, argparse
 
 from engine import utils
 
 
 class ToolKit(object):
 
-    def get_charcount(self, filepath):
+    def get_file(self, filepath):
         with open(filepath, 'r') as f:
             str_list = utils.get_file(f)
+        return str_list
+
+    def get_charcount(self, str_list):
         char_count = collections.Counter(''.join(str_list))
         return char_count
+
+    def calc_similarity(self, gen_words, ref_words):
+        # TODO: IDEA: Not implemented -- using ratio from Levenshtein library instead
+        #   Iterate through all words in the generated list and compare each generated word to ALL of the reference words
+        #   Generate a matrix for this comparison with all Levenshtein values
+        #   Use the comparison matrix to make claims about each word:
+        #       I.e. "This word is similar to x% of the reference material -- based on some threshold level of L. value
+        #   Aggregate the individual comparisons and normalize to make a general claim about the generated dataset
+        #       I.e. "This collection of words is similar to 40% of the reference material"
+        #           This would require the threshold value set in such a way that that it is agnostic of word length
+        #               percentage of characters similar? -- might get difficult with words of different lengths
+        #               NOTE: L.value as some ratio between total characters in words: len(word1) + len(word2)
+        # comp_matrix = []
+        # for word in gen_words:
+        #     result = [Levenshtein.distance(word, s) for s in ref_words]
+        pass
+
 
 
 if __name__ == "__main__":
     """
     Compare character counts and distributions between two list of passwords
+    USAGE:  python toolkit.py <file1> <file2>
+    OUTPUT: A ratio of similarity between the two wordlist files (Range of 0.0 to 1.0)
     """
-    file1 = "resources/___unvalidated.txt"
-    file2 = "resources/___validated_pwlen_fortraining.txt"
+
+    parser = argparse.ArgumentParser(description='Basic comparator for wordlists. Calculates the ratio of similarity.')
+    parser.add_argument('file1', type=str)
+    parser.add_argument('file2', type=str)
+
+    args = parser.parse_args()
+
+    generated_word_file = ""
+    reference_word_file = ""
+    if args.file1 and args.file2:
+        generated_word_file = args.file1
+        reference_word_file = args.file2
+    else:
+        parser.print_help()
+        exit()
 
     tk = ToolKit()
 
-    f1_count = tk.get_charcount(file1)
-    f2_count = tk.get_charcount(file2)
+    ref_word_list = tk.get_file(reference_word_file)
+    gen_word_list = tk.get_file(generated_word_file)
 
-    sorted_f1 = sorted(f1_count.items(), key=operator.itemgetter(1), reverse=True)
-    sorted_f2 = sorted(f2_count.items(), key=operator.itemgetter(1), reverse=True)
+    print(Levenshtein.seqratio(gen_word_list, ref_word_list))
 
-    for i in range(min(100, min(len(sorted_f1), len(sorted_f2)))):
-        print('%s\t%s' % (sorted_f1[i], sorted_f2[i]))
 
 
 
